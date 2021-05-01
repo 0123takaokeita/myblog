65c7c01cb3caa19e39d49b274d071648779b700d
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-01 14:32:17 +0900

    edit link

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 
   <!-- Bootstrap core JavaScript -->
-  <script src="http://myblog.local/wp-content/themes/myblog/vendor/jquery/jquery.min.js"></script>
-  <script src="http://myblog.local/wp-content/themes/myblog/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="http://myblog.local/wp-content/themes/myblog/js/clean-blog.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/js/clean-blog.min.js"></script>
   <?php wp_footer(); ?>
