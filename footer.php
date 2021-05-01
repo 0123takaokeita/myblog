bc74ef1e1878c71e84fc984ae7f7184e279188e7
takefumi <65272471+0123takaokeita@users.noreply.github.com>
2021-05-01 14:26:24 +0900

    footer parts

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="http://myblog.local/wp-content/themes/myblog/vendor/jquery/jquery.min.js"></script>
+  <script src="http://myblog.local/wp-content/themes/myblog/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="http://myblog.local/wp-content/themes/myblog/js/clean-blog.min.js"></script>
+  <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -120,14 +120,7 @@
       </div>
     </div>
   </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="http://myblog.local/wp-content/themes/myblog/vendor/jquery/jquery.min.js"></script>
-  <script src="http://myblog.local/wp-content/themes/myblog/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="http://myblog.local/wp-content/themes/myblog/js/clean-blog.min.js"></script>
-  <?php wp_footer(); ?>
+<?php get_footer(); ?>
 </body>
 
 </html>
